cf7ef32dfe3e6778dd27a59249e9baa453e5332c
TABLE2 <TABLE2>
2021-03-15 18:43:28 +0100

    #1 C# début message

---
+++ C#/RobotInterface/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RobotInterface
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        bool toggle = false;
+
+        private void buttonEnvoyer_Click(object sender, RoutedEventArgs e)
+        {
+            if(toggle)
+                buttonEnvoyer.Background = Brushes.Green;
+            else
+                buttonEnvoyer.Background = Brushes.Red;
+
+            toggle = !toggle;
+
+            SendMessage();
+        }
+
+        void SendMessage() {
+            string message = textBoxEmission.Text;
+            textBoxReception.Text = textBoxReception.Text + "Recu :" + message + "\n";
+            textBoxEmission.Text = "";
+        }
+
+        private void textBoxEmission_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter )
+                SendMessage();
+            }
+        }
+    }
+
